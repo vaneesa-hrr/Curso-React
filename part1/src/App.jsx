b780edf1da7b7b212ef4adb29270947447e3b75a
vaneesa.hrr <alumno.415193@iesazarquiel.es>
2025-03-10 12:55:48 +0100

    funcion App y utilizacion de console.log

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,14 @@
 const App = () => {
+  const now = Date();
+  const a = 10;
+  const b = 20;
+  console.log(now, a + b);
   return (
     <div>
       <p>Hello world</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   )
 };
