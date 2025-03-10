0e8c3ba331475b0c1aa1dbde3581ff36c6e8f382
vaneesa.hrr <alumno.415193@iesazarquiel.es>
2025-03-10 13:25:42 +0100

    no renderizar objetos

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,23 +1,15 @@
-const Hello = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const name = 'Peter';
-  const age = 10;
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <p>{friends[0]}</p>
+      <p>{friends[1]}</p>
     </div>
   )
 }
 
-export default App;
+export default App
