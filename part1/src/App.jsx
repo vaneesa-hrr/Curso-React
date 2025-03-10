3d613e32382958280b26df40d183f32686c2b0f5
vaneesa.hrr <alumno.415193@iesazarquiel.es>
2025-03-10 13:57:35 +0100

    ejercicio parte 1, renderiza componentes y usa props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,46 @@
-const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
-
+const Header = (props) => {
   return (
     <div>
-      <p>{friends[0].name} {friends[0].age} {friends[0].city}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <p>{props.course}</p>
     </div>
   )
 }
 
-export default App
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercise}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total} </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const ex1 = 10;
+  const part2 = 'Using props to pass data';
+  const ex2 = 7;
+  const part3 = 'State of a component';
+  const ex3 = 14;
+  const total = ex1 + ex2 + ex3;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercise={ex1} />
+      <Content part={part2} exercise={ex2} />
+      <Content part={part3} exercise={ex3} />
+      <Total total={total} />
+    </div>
+  )
+}
+
+export default App;
