ac1bac6a3481c2599f7368b0646c7343331d8676
vaneesa.hrr <alumno.415193@iesazarquiel.es>
2025-03-10 13:01:16 +0100

    renderizando el codigo

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,15 @@
+import React from 'react';
 const App = () => {
   const now = Date();
   const a = 10;
   const b = 20;
   console.log(now, a + b);
-  return (
-    <div>
-      <p>Hello world</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
-    </div>
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p', null, a, ' plus ', b, ' is ', a + b
+    )
   )
 };
 
